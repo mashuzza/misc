661bb74605257c90886205c5fe3eb141ba93b06c
Masha Logan <50681558+mashuzza@users.noreply.github.com>
2020-03-05 11:18:33 -0500

    Create odbc_write_SSMS.r

---
+++ odbc/odbc_write_SSMS.r
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(odbc)
+
+
+con <- dbConnect(odbc(), "AI1RSKSQLP01/RISKINSIGHT")
+str(con)
+
+
+# get the tables in 
+
+rtn_table<-read_csv("Z:/$POC_1$/Updated LN files/AI_8826AI_8826_DEP_RV5_IV_RTN_N.csv", 
+                    guess_max = 264164, na = c("N/A", "", "NA", "NULL", "<NA>", "-" ))
+
+summary(rtn_table)
+
+
+
+## replace (0.01) format to numeric
+
+rtn_table1<-rtn_table%>%mutate(EP = as.numeric(str_replace_all(rtn_table$EP, c('\\)'= '', '\\(' = '-', ','= ''))),
+                               INC_LOSS = as.numeric(str_replace_all(INC_LOSS, c('\\)'= '', '\\(' = '-', ','= ''))),
+                               ZIP = as.character(ZIP),
+                               EFFDT_enc = as.character(EFFDT_enc),
+                               EFF_MY = as.character(EFF_MY))
+
+
+## do the unorthodox thing and convert strings to factors
+
+## create a copy of the dataset
+rtn_data<-rtn_table1
+
+
+
+con <- dbConnect(odbc::odbc(), "AI1RSKSQLP01/RISKINSIGHT",
+                 Database = "LexisNexis", Driver = 'SQL Server')
+if (dbExistsTable(con, "AI_8826AI_8826_DEP_RV5_IV_RTN_N_updated"))
+  dbRemoveTable(con, "AI_8826AI_8826_DEP_RV5_IV_RTN_N_updated")
+dbWriteTable(con, name =  "AI_8826AI_8826_DEP_RV5_IV_RTN_N_updated",value = rtn_data)
+dbDisconnect(con)
+
