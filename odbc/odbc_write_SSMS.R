d48807f417b05d8026e6aee75df6728b8039aed0
Masha Logan <50681558+mashuzza@users.noreply.github.com>
2020-03-05 11:20:25 -0500

    Update odbc_write_SSMS.R

--- odbc/odbc_write_SSMS.R
+++ odbc/odbc_write_SSMS.R
@@ -29,12 +29,17 @@ rtn_table1<-rtn_table%>%mutate(EP = as.numeric(str_replace_all(rtn_table$EP, c('
 ## create a copy of the dataset
 rtn_data<-rtn_table1
 
-
+## write this table to your SSMS
 
 con <- dbConnect(odbc::odbc(), "AI1RSKSQLP01/RISKINSIGHT",
                  Database = "LexisNexis", Driver = 'SQL Server')
+## check if exists
 if (dbExistsTable(con, "AI_8826AI_8826_DEP_RV5_IV_RTN_N_updated"))
   dbRemoveTable(con, "AI_8826AI_8826_DEP_RV5_IV_RTN_N_updated")
+
+## write the table
 dbWriteTable(con, name =  "AI_8826AI_8826_DEP_RV5_IV_RTN_N_updated",value = rtn_data)
+
+## disconnect
 dbDisconnect(con)
 
