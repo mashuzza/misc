ee56192898184cc2b2cd14c186a6dcb1fd8ced23
Masha Logan <50681558+mashuzza@users.noreply.github.com>
2020-03-19 11:53:02 -0400

    Update ABC by transDt.sql

--- SQL/ABC Testing/ABC by transDt.sql
+++ SQL/ABC Testing/ABC by transDt.sql
@@ -1,6 +1,5 @@
-SELECT TransDt, count([PolicyNumber]) as NewABC
+SELECT TransDt,PolicyNumber
   FROM [ARS].[dbo].[ABC_Production]
   where year(transDt) = 2020
   and month(transDt) = 3
-  group by transdt
-  order by trans
+  order by 1 
